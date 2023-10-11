b41bd5ef289d79c3c5afd739ef92b975835aa6c6
pacheco-andrea <pacheco.gracia@gmail.com>
2023-10-11 16:28:54 +0200

    re-wrote and re-run of basic preprocessing for simple tenure cats + same projs + raster v for mapping

--- 000_gettingStarted.R
+++ 000_gettingStarted.R
@@ -8,67 +8,69 @@
 # libraries
 # Move on from any deprecated rgdal and raster parts of my workflow to sf and terra
 
-# install.packages("raster") # apparently raster is now simply importing terra, to prevent any code from breaking
 # install.packages("terra")
 # install.packages("rgdal")
 # install.packages("sf")
-# install.packages("fasterize")
 # install.packages("dplyr")
 # install.packages("ggplot2")
 # install.packages("RPostgreSQL")
 # install.packages("exactextractr")
 # install.packages("cowplot")
+# install.packages("blogdown")
 
-library(terra)
-library(sf)
-library(fasterize) # check this one - probably don't need it as the rasterize function prob works now
-library(ggplot2)
-library(dplyr)
 
-wdmain <- "N:/eslu/priv/pacheco/whoOwnsBRBD"
-#rasterOptions(tmpdir = "N:/eslu/priv/pacheco/biodivTenureBR/tmp", chunksize = 524288, maxmemory = 134217728)
-mode <- function(x, na.rm = FALSE) {
-  if(na.rm){
-    x = x[!is.na(x)]
-  }
-  ux <- unique(x)
-  return(ux[which.max(tabulate(match(x, ux)))])
+
+wdmain <- "N:/eslu/priv/pacheco/whoOwnsBRBD/"
+
+# function for reading in only specific columns
+read_my_shp = function(f){
+  s = st_read(f)
+  return(s[,c("X_uid_","tipo","uf", "geometry")]) # choose specific columns to keep lighter versions of these data: an identifier, the state, and the geometry
 }
 
+#rasterOptions(tmpdir = "N:/eslu/priv/pacheco/biodivTenureBR/tmp", chunksize = 524288, maxmemory = 134217728)
+# mode <- function(x, na.rm = FALSE) { # i dont think i need these anymore because the terra package is managing
+#   if(na.rm){
+#     x = x[!is.na(x)]
+#   }
+#   ux <- unique(x)
+#   return(ux[which.max(tabulate(match(x, ux)))])
+# }
+
 # LEARNING TO USE TERRA INSTEAD OF RASTER ----
-x <- rast()
-plot(x)
-x <- rast(ncol=36, nrow=18, xmin=-1000, xmax=1000,ymin=-100, ymax=900)
-res(x) <- 100
-crs(x) <- "+proj=utm +zone=48 +datum=WGS84"
-
-# values function:
-r <- rast(ncol=10, nrow=10)
-ncell(r)
-hasValues(r)
-values(r) <- 1:ncell(r)
-set.seed(0)
-values(r) <- runif(ncell(r))
-hasValues(r)
-sources(r) #check if in memory layers actually have cell values
-values(r)[10:20]
-plot(r, main = "raster with 100 cells")
-# other functions
-dim(r)
-xmax(r)
-
-# get the name of an example file installed with the package
-# do not use this construction of your own files
-filename <- system.file("ex/meuse.tif", package="terra")
-filename
-r <- rast(filename)
-plot(r)
-
-filename <- system.file("ex/logo.tif", package="terra")
-filename
-b <- rast(filename)
-b
-b[[2]] #layers on the raster still work with [[]]
+# x <- rast()
+# plot(x)
+# x <- rast(ncol=36, nrow=18, xmin=-1000, xmax=1000,ymin=-100, ymax=900)
+# res(x) <- 100
+# crs(x) <- "+proj=utm +zone=48 +datum=WGS84"
+# 
+# # values function: useful for counting values in the raster (instead of freq)
+# r <- rast(ncol=10, nrow=10)
+# ncell(r)
+# hasValues(r)
+# values(r) <- 1:ncell(r)
+# set.seed(0)
+# values(r) <- runif(ncell(r))
+# hasValues(r)
+# sources(r) #check if in memory layers actually have cell values
+# values(r)[10:20]
+# plot(r, main = "raster with 100 cells")
+# # other functions
+# dim(r)
+# xmax(r)
+# 
+# # get the name of an example file installed with the package
+# # do not use this construction of your own files
+# filename <- system.file("ex/meuse.tif", package="terra")
+# filename
+# r <- rast(filename)
+# plot(r)
+# 
+# filename <- system.file("ex/logo.tif", package="terra")
+# filename
+# b <- rast(filename)
+# b
+# b[[2]] #layers on the raster still work with [[]]
 
 
 # important/useful functions!
@@ -79,20 +81,20 @@ b[[2]] #layers on the raster still work with [[]]
 # resample (remember to not use this for creating a larger resolution - use aggregate for this.)
 
 # not sure i get how mask function works
-r <- rast(ncols=10, nrows=10)
-m <- rast(ncols=10, nrows=10)
-values(r) <- 1:100
-set.seed(1965)
-x <- round(3 * runif(ncell(r)))
-x[x==0] <- NA
-values(m) <- x
-mr <- mask(r,m) # so, this shows the mask function basically punches holes from the mask to the desired raster
+# r <- rast(ncols=10, nrows=10)
+# m <- rast(ncols=10, nrows=10)
+# values(r) <- 1:100
+# set.seed(1965)
+# x <- round(3 * runif(ncell(r)))
+# x[x==0] <- NA
+# values(m) <- x
+# mr <- mask(r,m) # so, this shows the mask function basically punches holes from the mask to the desired raster
 
 
 
 # rasterize Polygons
-f <- system.file("ex/lux.shp", package="terra")
-v <- vect(f)
-r <- rast(v, ncols=75, nrows=100)
-z <- rasterize(v, r, "NAME_2")
-plot(z)
+# f <- system.file("ex/lux.shp", package="terra")
+# v <- vect(f)
+# r <- rast(v, ncols=75, nrows=100)
+# z <- rasterize(v, r, "NAME_2")
+# plot(z)
---
+++ 00_preprocessBDandTenureData.R
@@ -0,0 +1,98 @@
+#### Biodiversity and Tenure in Brazil ####
+# script that reads updated version of land tenure data
+# and ubirajara's new biodiversity indicator data
+# output is simplified column versions of land tenure
+# and raster versions of the land tenure data in the projection of the biodiv data (this should only be used for mapping, not for accounting)
+# author: Andrea Pacheco
+# first run: 23.09.2022
+# last run: 10.10.2023
+
+# libraries
+library(terra)
+library(sf)
+library(ggplot2)
+library(dplyr)
+# 
+# library(exactextractr)
+# library(cowplot)
+# library(rasterVis)
+
+
+# load directories and etc.
+source("N:/eslu/priv/pacheco/whoOwnsBRBD/code/000_gettingStarted.R")
+
+# Tenure data ----
+# data is split into a .shp for each state. These were sourced from CSR UFMG and downloaded manually
+
+setwd(paste0(wdmain,"/data/raw/LandTenure_v20231009/"))
+l <- list.files()
+files <- l[-c(grep(".toml", l))]
+
+# 1. read and write out simpler files # HERE FIGURE OUT IF I SHOULD WRITE OUT AS JSON OR SQLITE????
+# make sure to keep state-by-state structure in order to facilitate future parallelization 
+
+
+# loop through reading and writing simplified shapefiles
+for (i in 1:length(unique(files[grep(".shp",files)])))
+{
+  setwd(paste0(wdmain,"/data/raw/LandTenure_v20231009/"))
+  shps <- grep(".shp", files)  # read in all the shapefiles (with only specific columns established in the read_my_json funct above), bind rows into one df
+  # read one shape at a time
+  stateShp <- read_my_shp(files[shps[i]])
+  # keep same name
+  name <- paste(files[shps[i]])
+  # write out
+  setwd(paste0(wdmain,"/data/processed/landTenureCategs_v2023/"))
+  st_write(stateShp, name)
+  print(i)
+}
+
+# Biodiversity indicator data ----
+setwd(paste0(wdmain,"/data/raw/Biodiversity_v20231009"))
+list.files()
+richness <- rast("Species_Richness_All_groups_MARS.tif")
+richness
+plot(richness)
+res(richness) # .01*.01degrees - which means at ~1km. Now it's in WGS84. 
+# transform to south america albers equal area for making calculations 
+# test first with one:
+my_crs <- terra::crs(stateShp, proj = TRUE)
+# richness_eq <- project(richness, my_crs)
+# plot(richness_eq)
+
+setwd(paste0(wdmain,"/data/raw/Biodiversity_v20231009"))
+rastFiles <- grep(".tif$", list.files())
+list.files()[rastFiles]
+for(i in 1:length(rastFiles))
+{
+  setwd(paste0(wdmain,"/data/raw/Biodiversity_v20231009"))
+  r <- rast(paste0(list.files()[rastFiles][i]))
+  r2 <- project(r, my_crs)
+  name <- gsub(".tif", "_SAalbers.tif", paste0(list.files()[rastFiles][i]))
+  print(i)
+  setwd(paste0(wdmain,"/data/processed/BiodivIndicators_SA-albers"))
+  writeRaster(r2, filename = name)
+}
+
+# rasterize tenure data ----
+# THIS SHOULD BE FOR PLOTTING MAPS - NOT FOR CALCULATIONS!
+# make mask
+mask <- r2*0
+
+setwd(paste0(wdmain,"/data/processed/landTenureCategs_v2023/"))
+# make a raster for each of the shapes
+rlist <- list()
+for (i in 1:length(grep(".shp",list.files()))) 
+{
+  shps <- grep(".shp", list.files())  
+  stateShp <- read_my_shp(list.files()[shps[i]])
+  rlist[[i]] <- rasterize(stateShp, mask, "tipo")
+}
+# merge individual states into one whole map of brazil
+br <- merge(sprc(rlist)) # needs to be a SpatRaster Collection first 
+plot(br) # it's fine that is numerizes categories because categories will be numbered alphabetically.
+setwd(paste0(wdmain,"/data/processed/landTenureCategsRaster/"))
+writeRaster(br, filename = "landTenure_AST-IRU-PCT_SAalbers_1km.tif")
+
+# next script: extract biodiversity values per property
+
