806b5cb7e8c063f1fa6c536e367887b2b65a38f3
pacheco-andrea <pacheco.gracia@gmail.com>
2024-01-10 13:24:47 +0100

    testing geo functs

--- 00_preprocessTenureData.R
+++ 00_preprocessTenureData.R
@@ -22,54 +22,45 @@ setwd(paste0(wdmain,"/data/processed/"))
 grep("^landTenure", list.files())
 tenfolds <- list.files()[grep("^landTenure_", list.files())]
 
-# get data inside each folder, print the column names
-# why would they need to have the same column names though?
 
-setwd(paste0(wdmain,"/data/processed/", tenfolds[i]))
-s <- grep(".shp", list.files())
-list.files()[s]
-
-data <- st_read("landTenure_indigenous_20231212_SAalbers.shp")
-colnames(data)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[2]))
-data2 <- st_read("cf_imoveis_imaflora_ac.shp")
-colnames(data2)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[3]))
-data3 <- st_read("landTenure_RPPN_20231212_SAalbers.shp")
-colnames(data3)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[4]))
-data4 <- st_read("landTenure_SIGEFproperties_20231312_SAalbers.shp")
-colnames(data4)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[5]))
-data5 <- st_read("landTenure_SNCIproperties_20240105_SAalbers.shp")
-colnames(data5)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[6]))
-data6 <- st_read("landTenure_UCs_MMA_20231212_SAalbers.shp")
-colnames(data6)
-
-setwd(paste0(wdmain,"/data/processed/", tenfolds[7]))
-data7 <- st_read("landTenure_Undesignated-Other-Military_SAalbers.shp")
-colnames(data7)
-
-
-
-# let me try subsetting indigenous that are inside PAs
+# figure out spatial operations i need to use: ----
+# try subsetting indigenous that are inside PAs
 tenfolds
 
+# intersect vs. intersection?
 testintersect <- st_intersects(ind, uc)
 testintersection  <- st_intersection(uc, ind) # returns geometry of the shared portion of x and y
-nrow(testintersection )
-plot(testintersection $geometry)
+nrow(testintersection)
+plot(testintersection$geometry)
 
-testwithin <- st_snap(ind, uc, tolerance = 1000) # run time start ~10:45
+# snapping?
+testsnap <- st_snap(ind, uc, tolerance = 1000) # run time start ~10:45
+plot(testsnap$geometry)
+setwd(paste0(wdmain, "data/tmp"))
+st_write(testsnap, "test_indigenousSnappedtoUCs.shp")
+testsnap2 <- st_buffer(testsnap, dist=0)
+# compare snapping result with previous number of intersections
+a_int <- st_intersection(testsnap2, uc)
+nrow(a_int)
+nrow(testintersection)
 
-testoverlap <- st_overlap(ind, uc # ids if x and y share space, are of the same dimension but are not completely contained
+par(mfrow = c(1,2))
+plot(ind$geometry)
+plot(testsnap2$geometry)
+# there are even more intersections after i snapped indigenous to ucs!
+# ok i think I understand a bit better now, the snapping isn't going to work universally for all UCs it could just work for those 
+# where the boundaries should be each other...
+par(mfrow = c(1,1))
 
+# overlap - should quantify amount of overlap
+testoverlap <- st_overlaps(ind, uc) # ids if x and y share space, are of the same dimension, but are not completely contained by each other
+
+# the easiest way I can think of prioritizing is 
+# 1. create indigenous that doesn't touch ucs
+ind_difference <- st_difference(ind, uc)
+test_difference <- st_intersection(ind_difference, uc)
+
+# ----
 # ALL OF THIS FOR THE ONE WALL-TO-WALL with the minimum necessary of overlaps (as this will become one category)                       
 # i need to come up with a series of rules
 # if UCs and IND overlap: should resolve, prioritize UCs
@@ -84,7 +75,7 @@ testoverlap <- st_overlap(ind, uc # ids if x and y share space, are of the same 
 # 
 
 
-# TEST:
+# TEST: ----
 set.seed(131)
 
 m = rbind(c(0,0), c(1,0), c(1,1), c(0,1), c(0,0))
@@ -100,7 +91,15 @@ title("overlapping squares")
 # functions
 d = st_difference(s) # sequential differences: s1, s2-s1, s3-s2-s1, ...
 plot(d, col = sf.colors(categorical = TRUE, alpha = .5))
-title("non-overlapping differences")
+title("non-overlapping differences") # for these differences, is there no way to determine the order? i guess i could manually think about reordering the datasets
+# im pretty sure what i need for prioritizing Ucs above indigenous is the difference operation
+# so that Ucs take a bite out of indigenous, and then i can plot them without overlaps
+
+# determine the order: sort by categories in the columns!! (but this is for one data set, would i have to join them first?)
+# e.g. wdpa2 <- wdpa2 >%> arrange(category, year)
+
+
+
 i = st_intersection(s) # all intersections
 plot(i, col = sf.colors(categorical = TRUE, alpha = .5))
 title("non-overlapping intersections") # i don't get it. 
