32850c21a927ee3e41025bda62cc0b7d1cf133fd
pacheco-andrea <pacheco.gracia@gmail.com>
2024-10-08 11:38:59 +0200

    reclassified LC rasters to forest cover

--- 04_preprocessForestCovData.R
+++ 04_preprocessForestCovData.R
@@ -18,29 +18,65 @@ source("N:/eslu/priv/pacheco/whoOwnsBRBD/code/000_gettingStarted.R")
 
 
 # Biodiversity indicators ----
-setwd(paste0(wdmain,"/data/raw/Biodiversity_v202410"))
+setwd(paste0(wdmain,"/data/raw/landCover_mapbiomas"))
 rasters <- list.files()[grep("tif$", list.files())]
 rasters
-richness <- rast(rasters[grep("Richness", rasters)])
-endemism <- rast(rasters[grep("Endemism", rasters)])
-phylogenetic <- rast(rasters[grep("Phylodiversity", rasters)])
+landCover <- rast(rasters)
+res(landCover) # .0002 degrees 
 
-par(mfrow = c(1,3))
-plot(richness)
+# reclassify this raster into forest, agriculture, and other
 
-b_at <- richness[[1]]-richness[[2]]
-plot(b_at)
-par(mfrow = c(1,2))
-plot(b_at)
-plot(richness[[3]])
-dev.off()
+reclass_matrix <- matrix(c(
+  1, 10,    # Category 1: values 1 -> 10
+  3, 10,    # Category 1: values 3 -> 10
+  4, 10,    # Category 1: values 4 -> 10
+  5, 10,    # Category 1: values 5 -> 10
+  6, 10,    # Category 1: values 6 -> 10
+  49, 10,   # Category 1: values 49 -> 10
+  10, 10,   # Category 1: values 10 -> 10
+  11, 10,   # Category 1: values 11 -> 10
+  12, 10,   # Category 1: values 12 -> 10
+  32, 10,   # Category 1: values 32 -> 10
+  29, 10,   # Category 1: values 29 -> 10
+  50, 10,   # Category 1: values 50 -> 10
+  14, 20,   # Category 2: values 14 -> 20
+  15, 20,   # Category 2: values 15 -> 20
+  18, 20,   # Category 2: values 18 -> 20
+  19, 20,   # Category 2: values 19 -> 20
+  39, 20,   # Category 2: values 39 -> 20
+  20, 20,   # Category 2: values 20 -> 20
+  40, 20,   # Category 2: values 40 -> 20
+  62, 20,   # Category 2: values 62 -> 20
+  41, 20,   # Category 2: values 41 -> 20
+  36, 20,   # Category 2: values 36 -> 20
+  46, 20,   # Category 2: values 46 -> 20
+  47, 20,   # Category 2: values 47 -> 20
+  35, 20,   # Category 2: values 35 -> 20
+  48, 20,   # Category 2: values 48 -> 20
+  9, 20,    # Category 2: values 9 -> 20
+  21, 20   # Category 2: values 21 -> 20
+), ncol = 2, byrow = TRUE)
+
+# Reclassify the raster
+# u <- unique(landCover[[1]])
+forestCover85 <- terra::classify(landCover[[1]], reclass_matrix, others = 30)
+u85 <- unique(forestCover85)
+
+forestCover23 <- terra::classify(landCover[[2]], reclass_matrix, others = 30)
+u23 <- unique(forestCover23)
 
 
-res(richness) # .01 degrees 
-# map and write out richness map
-summary(richness)
-summary(endemism)
-plot(richness)
+# Save the reclassified raster (optional)
+setwd(paste0(wdmain,"/data/processed/forestCover"))
+writeRaster(forestCover85, "forestCover1985.tif", filetype="GTiff", overwrite=TRUE)
+writeRaster(forestCover23, "forestCover2023.tif", format="GTiff", overwrite=TRUE)
+
+
+# CONTINUE HERE
+# map and write out a map of forest cover in 1985, and 2023
+plot(forestCover85)
+
+
 
 breaks <- classIntervals(values(richness, na.rm = T))
 breaks$brks
