567cd7acabb51ad90c738b402f1dfffa7dbce4e4
pacheco-andrea <pacheco.gracia@gmail.com>
2024-10-10 12:02:49 +0200

    adjusted reclass, trying to add

--- 04_preprocessForestCovData.R
+++ 04_preprocessForestCovData.R
@@ -57,19 +57,49 @@ reclass_matrix <- matrix(c(
   21, 20   # Category 2: values 21 -> 20
 ), ncol = 2, byrow = TRUE)
 
-# Reclassify the raster
+# Reclassify the 1985 raster
 # u <- unique(landCover[[2]])
-# forestCover85 <- terra::classify(landCover[[1]], reclass_matrix, others = 30)
+forestCover85 <- terra::classify(landCover[[1]], reclass_matrix, others = 0)
 # u85 <- unique(forestCover85)
-# 
-# forestCover23 <- terra::classify(landCover[[2]], reclass_matrix, others = 30)
+setwd(paste0(wdmain,"/data/processed/forestCover"))
+writeRaster(forestCover85, "forestCover1985.tif", filetype="GTiff", overwrite=TRUE)
+
+# reclassify the 2023 raster
+reclass_matrix <- matrix(c(
+  1, 1,    # Category 1: values 1 -> 10
+  3, 1,    # Category 1: values 3 -> 10
+  4, 1,    # Category 1: values 4 -> 10
+  5, 1,    # Category 1: values 5 -> 10
+  6, 1,    # Category 1: values 6 -> 10
+  49, 1,   # Category 1: values 49 -> 10
+  10, 1,   # Category 1: values 10 -> 10
+  11, 1,   # Category 1: values 11 -> 10
+  12, 1,   # Category 1: values 12 -> 10
+  32, 1,   # Category 1: values 32 -> 10
+  29, 1,   # Category 1: values 29 -> 10
+  50, 1,   # Category 1: values 50 -> 10
+  14, 2,   # Category 2: values 14 -> 20
+  15, 2,   # Category 2: values 15 -> 20
+  18, 2,   # Category 2: values 18 -> 20
+  19, 2,   # Category 2: values 19 -> 20
+  39, 2,   # Category 2: values 39 -> 20
+  20, 2,   # Category 2: values 20 -> 20
+  40, 2,   # Category 2: values 40 -> 20
+  62, 2,   # Category 2: values 62 -> 20
+  41, 2,   # Category 2: values 41 -> 20
+  36, 2,   # Category 2: values 36 -> 20
+  46, 2,   # Category 2: values 46 -> 20
+  47, 2,   # Category 2: values 47 -> 20
+  35, 2,   # Category 2: values 35 -> 20
+  48, 2,   # Category 2: values 48 -> 20
+  9, 2,    # Category 2: values 9 -> 20
+  21, 2   # Category 2: values 21 -> 20
+), ncol = 2, byrow = TRUE)
+forestCover23 <- terra::classify(landCover[[2]], reclass_matrix, others = 0)
 # u23 <- unique(forestCover23)
+setwd(paste0(wdmain,"/data/processed/forestCover"))
+writeRaster(forestCover23, "forestCover2023.tif", filetype="GTiff", overwrite=TRUE)
 
-
-# Save the reclassified raster (optional)
-# setwd(paste0(wdmain,"/data/processed/forestCover"))
-# writeRaster(forestCover85, "forestCover1985.tif", filetype="GTiff", overwrite=TRUE)
-# writeRaster(forestCover23, "forestCover2023.tif", filetype="GTiff", overwrite=TRUE)
 # reread in
 setwd(paste0(wdmain,"/data/processed/forestCover"))
 f23 <- rast("forestCover2023.tif")
@@ -79,31 +109,22 @@ terra::plot(f23)
 
 # Map: ----
 # i need a map that is 1) red 10-f85 that is now 20-ag23, green all 10-23
-# meants that first 30 == 0
-reclass_matrix <- matrix(c(30, 0), ncol = 2, byrow = TRUE)
-f23_2 <- terra::classify(f23, reclass_matrix)
-plot(f23_2)
-
-reclass_matrix <- matrix(c(30, 0,
-                           20, 2,
-                           10, 1), ncol = 2, byrow = TRUE)
-f85_2 <- terra::classify(f85, reclass_matrix)
-plot(f85_2)
-
 f2ag <- f23_2 + f85_2
-plot(f2ag)
-# write out
 setwd(paste0(wdmain,"/data/processed/forestCover"))
 writeRaster(f2ag, "forest2agriculture.tif", filetype="GTiff", overwrite=TRUE)
-# plot map 
-u <- unique(f2ag)
+f2ag <- rast("forest2agriculture.tif")
 
-f2agCols <- data.frame(0, "#ffffff",
-                       20, "#ffffff", 
+u <- unique(f2ag)
+plot(f2ag)
+
+# plot map 
+
+f2agCols <- data.frame(0, "gray50",
+                       20, "gray50", 
                        10, "green", 
                        11, "green",
                        21, "red",
-                       22, "#ffffff",
+                       22, "gray50",
                        12, "green")
 terra::plot(f2ag, 
             type = "classes",
@@ -112,58 +133,13 @@ terra::plot(f2ag,
             col = f2agCols)
 
 
-
-
-breaks <- classIntervals(values(richness, na.rm = T))
-breaks$brks
-
 setwd(paste0(wdmain, "output/maps/"))
 png("richness_v20241004.png", units = "px", width = 1500, height = 1500, res = 300)
-terra::plot(richness[[1]], 
-     type = "continuous",
-     range = c(min(values(richness), na.rm = T),max(values(richness), na.rm = T)),
-     breaks = breaks$brks,
-     axes=F,
-     mar = c(0, 0, 0, 7.1),
-     col = viridis(n = as.integer(max(values(richness), na.rm = T)-min(values(richness), na.rm = T)), direction = -1))
+terra::plot(f2ag, 
+            type = "classes",
+            axes=F,
+            mar = c(0, 0, 0, 7.1),
+            col = f2agCols)
 dev.off()
 
 
-# map and write out endemism map
-summary(endemism)
-plot(endemism)
-
-breaks <- classIntervals(values(endemism, na.rm = T))
-breaks$brks
-
-
-setwd(paste0(wdmain, "output/maps/"))
-png("endemism_v20241004.png", units = "px", width = 1500, height = 1500, res = 300)
-terra::plot(endemism[[1]], 
-     type = "continuous",
-     range = c(0,1),
-     breaks = breaks$brks,
-     axes=F,
-     mar = c(0, 0, 0, 7.1),
-     col = magma(n=1000, direction = -1))
-dev.off()
-
-# map and write out phylogenetic diversity map
-summary(phylogenetic)
-plot(phylogenetic)
-
-breaks <- classIntervals(values(phylogenetic, na.rm = T))
-breaks$brks
-
-setwd(paste0(wdmain, "output/maps/"))
-png("phylodiversity_v20240819.png", units = "px", width = 1500, height = 1400, res = 300)
-terra::plot(phylogenetic[[1]], 
-     type = "continuous",
-     range = c(min(values(phylogenetic), na.rm = T),max(values(phylogenetic), na.rm = T)),
-     breaks = breaks$brks,
-     axes=F,
-     mar = c(0, 0, 0, 7.1),
-     col = magma(n=1000, direction = -1))
-dev.off()
-
-# writing out re-projected data to use for extractions never worked, so this is unnecessary
